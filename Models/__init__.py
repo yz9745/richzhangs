4812ea112495bb147684421c762a725d944d49d0
minglun <minglun.zhang@duke.edu>
2021-10-06 23:36:28 -0400

    unfinished

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,41 +1,50 @@
-import pandas as pd
-from executions import *
-from filters import *
+# import pandas as pd
+# from util import *
+import yfinance as yf
 
-ledger, blotter = []
-active_list = []
-
-def initilization():
-    blotter = pd.DataFrame(None, columns = ['ID', 'Date', 'Time', 'Ticker', 'Action', 'Price', 'Type', 'Status'])
-    ledger = pd.DataFrame(None, columns = ['ID', 'Date', 'Time', 'Active Holdings', 'Cash', 'PnL'])
-    active_list = pd.DataFrame(None, columns = ['ID','Ticker', 'Holding Status', 'Current Price', 'Holding price', 'Position', 'Target', 'Stop', 'PnL'])
+# ledger, blotter = []
+# holding_list, order_list = []
+# start, end, cash = 0
 
 
-
-def back_test(start_date, end_date):
-    start = start_date.toInt()
-    end = end_date.toInt()
-    while start < end:
-        if len(active_list) >= 10:
-            pass
-        else:
-            list = myFilter(start)
-            for stock_info in list:
-                open_position()
-    return
+# def init():
+#     blotter = pd.DataFrame(None, columns=['ID', 'Date', 'Time', 'Ticker', 'Action', 'Price', 'Type', 'Status'])
+#     ledger = pd.DataFrame(None, columns=['ID', 'Date', 'Time', 'Active Holdings', 'Cash', 'PnL'])
+#     holding_list = pd.DataFrame(None,
+#                                 columns=['ID', 'Ticker', 'Holding Status', 'Current Price', 'Holding price', 'Position',
+#                                          'Target', 'Stop', 'PnL'])
+#     order_list = pd.DataFrame(None, columns=['ID', 'Ticker', 'Action', 'Limit Price'])
+#     start = 0
+#     end = 365
+#     cash = 10000
+#     record()
+#
+#
+# def back_test():
+#     # record the beginning status
+#     while start < end:
+#         close_positions()  # close the position which meets the condition
+#         if len(active_list) >= 15:
+#             pass
+#         else:
+#             open_positions()  # get the limited orders done
+#         updateOrderList()  # update next day trading targets
+#         record()
+#         start += 1
+#     return
 
 
 def main():
-    initilization()
-    back_test('2020-1-1', '2021-1-1')
+    # init() # ledger, blotter initialization
+    # # sql_init(), may store the processed data in the database
+    # data_process('./data.csv') # deal with the raw data, generate indicators and signals
+    # back_test()
+    x = yf.Ticker("X")
+    hist = x.history(start="2019-01-01", end="2021-01-01")
+    hist.to_excel("X_2y_Data.xlsx")
+    print(hist)
 
 
 if __name__ == "__main__":
     # execute only if run as a script
     main()
-
-
-
-
-
-
