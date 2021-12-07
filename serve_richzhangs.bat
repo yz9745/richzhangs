ab36a306dd0f5259f3ed17030a1edc570b0f521c
richzhangs <richzhangs@protonmail.com>
2021-12-07 10:23:37 -0500

    add serve_richzhangs.bat

---
+++ serve_richzhangs.bat
@@ -0,0 +1,4 @@
+cd C:\Users\vcm\Desktop\FINTECH.590
+:: git pull https://%RICHZHANGS_PAT%@github.com/yz9745/richzhangs.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
